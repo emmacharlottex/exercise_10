4c2c75489efbe95335661067faa03fa48abf3bd0
Ayishat Adedeji <ayomilesi21@gmail.com>
2025-01-24 23:29:05 +0000

    EDIT: Working on homework, step 5 and 6 of question 1

--- ex10_starter.py
+++ ex10_starter.py
@@ -6,14 +6,18 @@ if sys.platform == 'win32':
 else:
     hdir = os.environ['HOME']
 
-print(sys.platform)
+
 # Construct a portable wildcard pattern
 pattern = os.path.join(hdir,'*')
+search_dir = 'C:\Code\GetIntoTech\PycharmProjects\Exercise10'
 
 # TODO: Use the glob.glob() function to obtain the list of filenames
+for file in glob.glob(search_dir):
+    print(search_dir)
 
 # TODO: use os.path.getsize to find each file's size
-
+size = os.path.getsize(file)
+print(size)
 # TODO: Add a test to only display files that are not zero length
 
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
